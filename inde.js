61fdb4909d3af103a7af00894b4d7b8dee2a5a8b
SUBAcodes <boxasuxa@gmail.com>
2023-04-29 16:55:04 +0500

    module events

---
+++ inde.js
@@ -0,0 +1,9 @@
+const os = require('os')
+
+// console.log(os.platform());     platforma yani win32  
+
+// console.log(os.cpus());      malumotlar
+
+// console.log(os.arch());     arxiv
+
+// console.log(os.freemem());   Xotira malumoti
