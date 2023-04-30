5c65741db4a3a3caa2c8f30350f8b66df080b4ba
SUBAcodes <boxasuxa@gmail.com>
2023-04-30 10:17:49 +0500

    Html bilan ishlash

---
+++ workHtml.js
@@ -0,0 +1,46 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const server = http.createServer((req, res) => {
+    if(req.method === "GET"){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+
+        if(req.url === '/'){
+            fs.readFile(path.join(__dirname, 'templates', 'index.html'), 'utf-8', (err, conten) =>{
+                if(err) throw err
+                res.end(conten)
+            })
+        }else if(req.url === '/about'){
+            fs.readFile(path.join(__dirname, 'templates', 'about.html'), 'utf-8', (err, conten) =>{
+                if(err) throw err
+                res.end(conten)
+            })
+        }else if(req.url === '/cantact'){
+            fs.readFile(path.join(__dirname, 'templates', 'cantact.html'), 'utf-8', (err, conten) =>{
+                if(err) throw err
+                res.end(conten)
+            })
+        }
+
+    }else if(req.method === "POST"){
+        const body = []
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+
+        req.on('end', ()=>{
+            const message = body.toString().split('=')[1]
+
+            res.end(`Name successfully added: ${message}`)
+        })
+    }
+})
+
+
+server.listen(3000, () => {
+    console.log('Server has been sstarted on port: 3000');
+})
+
