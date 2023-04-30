fba509c36c8070d95ba4dac4c98b6339993ad499
SUBAcodes <boxasuxa@gmail.com>
2023-04-30 10:45:38 +0500

    CLI bilan ishlash

---
+++ moduls/cli.js
@@ -0,0 +1,11 @@
+const logger = () => {
+    const resp = {}
+
+    for(let i = 2; i<process.argv.length; i++){
+        const arg = process.argv[i].split('=')
+        resp[arg[0]] = arg[1] ? arg[1] : true
+    }
+    return resp
+}
+
+console.log(logger());
